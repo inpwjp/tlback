fe46ff9ad9f8461fa6b473361d036d57cdc29b81
inpwjp <inpw@mua.biglobe.ne.jp>
2017-05-10 14:14:18 +0900

    fixed test

--- test/test_tlbk.rb
+++ test/test_tlbk.rb
@@ -5,14 +5,6 @@ require './tlback.rb'
 # require '../tlback.rb'
 
 class TestTlbk < ActiveSupport::TestCase
-  def setup
-    settings = load_settings
-    @test_connection = PG::connect(host: settings["postgresql"]["host"], user: settings["postgresql"]["user"],password: settings["postgresql"]["password"], dbname: settings["postgresql"]["dbname"], port: settings["postgresql"]["port"])
-  end
-
-  def teardown
-    @test_connection.finish if ! @test_conection.nil?
-  end
 
   def test_get_client
     client = get_client
@@ -20,10 +12,16 @@ class TestTlbk < ActiveSupport::TestCase
   end
 
   def test_backup_tl
-    old_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
-    assert_nothing_raised{ backup_tl }
-    new_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
-    assert_block{ old_count[0]["count"] < new_count[0]["count"] }
+    begin
+      settings = load_settings
+      @test_connection = PG::connect(host: settings["postgresql"]["host"], user: settings["postgresql"]["user"],password: settings["postgresql"]["password"], dbname: settings["postgresql"]["dbname"], port: settings["postgresql"]["port"])
+      old_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
+      assert_nothing_raised{ backup_tl }
+      new_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
+      assert_block{ old_count[0]["count"] < new_count[0]["count"] }
+    ensure
+      @test_connection.finish if ! @test_conection.nil?
+    end
   end
 
 end
