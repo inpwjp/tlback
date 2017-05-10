a737858427380fbf9cb26d887d63dd3c2271fea6
inpwjp <inpw@mua.biglobe.ne.jp>
2017-05-10 13:35:32 +0900

    update test

--- test/test_tlbk.rb
+++ test/test_tlbk.rb
@@ -5,13 +5,25 @@ require './tlback.rb'
 # require '../tlback.rb'
 
 class TestTlbk < ActiveSupport::TestCase
+  def setup
+    settings = load_settings
+    @test_connection = PG::connect(host: settings["postgresql"]["host"], user: settings["postgresql"]["user"],password: settings["postgresql"]["password"], dbname: settings["postgresql"]["dbname"], port: settings["postgresql"]["port"])
+  end
+
+  def teardown
+    @test_connection.finish
+  end
+
   def test_get_client
     client = get_client
     assert_true(client.present?)
   end
 
   def test_backup_tl
-    assert_nothing_raised(backup_tl)
+    old_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
+    assert_nothing_raised{ backup_tl }
+    new_count = @test_connection.exec("SELECT count(*) FROM TIMELINE")
+    assert_block{ old_count[0]["count"] < new_count[0]["count"] }
   end
 
 end
